0d27a101f7217f30532dcb8427ea1443d28f8ce0
Angga Bawole <bawoleangga03@gmail.com>
2021-06-11 17:35:21 +0800

    install auth

--- resources/views/includes/admin/style.blade.php
+++ resources/views/includes/admin/style.blade.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css">
 <link rel="stylesheet" href="{{ url('frontend-admin/assets/vendors/css/vendor.bundle.base.css') }}">
 <link rel="stylesheet" href="{{ url('frontend-admin/assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ url('frontend-admin/assets/css/new.css') }}">
---
+++ resources/views/layouts/logg.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ url('frontend/images/favicon.ico') }}">
+  <title>@yield('title')</title>
+
+  @stack('prepend-style')
+  @include('includes.style')
+  {{-- @livewireStyles --}}
+  @stack('addon-style')
+
+</head>
+<body>
+
+  @yield('content')
+
+  
+  @stack('prepend-script')
+  @include('includes.script')
+  {{-- @livewireScripts --}}
+  @stack('addon-script')
+</body>
+</html>
